9d732eaa69e4265749c3aa8b677f173d4b3c13f8
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-06-19 12:01:41 -0400

    Add FormGroup component.

---
+++ client/js/components/FormGroup.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { formUpdate } from '../actions/formActions';
+
+const FormGroup = ({
+    children,
+    label,
+    id,
+    activeForm,
+    formChange,
+}) => {
+    return (
+        <div className="form-group">
+            <label htmlFor={id} className="form-label">{label}</label>
+            {
+                React.cloneElement(
+                    children,
+                    {
+                        id,
+                        value: activeForm[id] || '',
+                        onChange: formChange(id),
+                    },
+                )
+            }
+        </div>
+    );
+};
+
+FormGroup.propTypes = {
+    children: React.PropTypes.element.isRequired,
+    label: React.PropTypes.string.isRequired,
+    id: React.PropTypes.string.isRequired,
+    activeForm: React.PropTypes.object.isRequired,
+    formChange: React.PropTypes.func.isRequired,
+};
+
+const mapState = ({ activeForm }) => {
+    return {
+        activeForm,
+    };
+};
+
+const mapDispatch = (dispatch) => {
+    return {
+        formChange(field) {
+            return (e, ...args) => {
+                const value = typeof e.target.value !== 'undefined' ? e.target.value : args[1];
+                const update = {
+                    [field]: value,
+                };
+                dispatch(formUpdate(update));
+            };
+        },
+    };
+};
+
+
+export default connect(mapState, mapDispatch)(FormGroup);
