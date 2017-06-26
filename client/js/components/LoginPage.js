ce8364d7578460a2f07c37f1eaa6d19c101d9d46
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-06-26 16:46:37 -0400

    Left buttons show appropriate form.

--- client/js/components/LoginPage.js
+++ client/js/components/LoginPage.js
@@ -11,21 +11,34 @@ import {
     register,
 } from '../actions/apiActions';
 
+import {
+    formInit,
+    formUpdate,
+} from '../actions/formActions';
+
 class Login extends React.Component {
     static propTypes = {
         activeForm: React.PropTypes.object,
+        onComponentDidMount: React.PropTypes.func,
+        changeSelection: React.PropTypes.func,
         onLogin: React.PropTypes.func,
         onRegister: React.PropTypes.func,
-    };
+    }
+
+    componentDidMount() {
+        this.props.onComponentDidMount();
+    }
 
     render() {
         const {
             activeForm,
+            changeSelection,
             onLogin,
             onRegister,
         } = this.props;
 
         const {
+            login_choice,
         } = activeForm;
 
         return (
@@ -36,102 +49,109 @@ class Login extends React.Component {
                     <div className="container">
                         <div className="col-sm-3 left-list">
                             <div className="row">
-                                <a href="">
+                                <button id="login" onClick={changeSelection('login')} >
                                     Login
                                     <i className="fa fa-chevron-right pull-right" aria-hidden="true" />
-                                </a>
+                                </button>
                             </div>
                             <div className="row">
-                                <a href="">
+                                <button id="register" onClick={changeSelection('register')} >
                                     Register
                                     <i className="fa fa-chevron-right pull-right" aria-hidden="true" />
-                                </a>
+                                </button>
                             </div>
                             <div className="row">
-                                <a href="">
+                                <button id="password" onClick={changeSelection('password')} >
                                     Forgot password
                                     <i className="fa fa-chevron-right pull-right" aria-hidden="true" />
-                                </a>
+                                </button>
                             </div>
                             <div className="row">
-                                <a href="">
+                                <button id="username" onClick={changeSelection('username')} >
                                     Forgot username
                                     <i className="fa fa-chevron-right pull-right" aria-hidden="true" />
-                                </a>
+                                </button>
                             </div>
                         </div>
                         <div className="col-sm-7 col-sm-offset-1" id="login-select">
-                            <h2>LOGIN</h2>
-                            <form onSubmit={onLogin}>
-                                <fieldset>
-                                    <div className="row">
-                                        <div className="col-sm-12 col-md-7">
-                                            <FormGroup label="Username" id="username">
-                                                <input type="text" className="form-control" placeholder="Username" aria-label="Username" />
-                                            </FormGroup>
+                            { login_choice === 'login' ? (
+                                <div>
+                                    <h2>LOGIN</h2>
+                                    <form onSubmit={onLogin}>
+                                        <fieldset>
+                                            <div className="row">
+                                                <div className="col-sm-12 col-md-7">
+                                                    <FormGroup label="Username" id="username">
+                                                        <input type="text" className="form-control" placeholder="Username" aria-label="Username" />
+                                                    </FormGroup>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="col-sm-12 col-md-7">
+                                                    <FormGroup label="Password" id="password">
+                                                        <input type="password" className="form-control" placeholder="Password" aria-label="Password" />
+                                                    </FormGroup>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                        <div className="row">
+                                            <div className="col-sm-12 col-md-offset-3 col-xs-6">
+                                                <button className="btn btn-lex">Login</button>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-sm-12 col-md-7">
-                                            <FormGroup label="Password" id="password">
-                                                <input type="password" className="form-control" placeholder="Password" aria-label="Password" />
-                                            </FormGroup>
-                                        </div>
-                                    </div>
-                                </fieldset>
-                                <div className="row">
-                                    <div className="col-sm-12 col-md-offset-3 col-xs-6">
-                                        <button className="btn btn-lex">Login</button>
-                                    </div>
+                                    </form>
                                 </div>
-                            </form>
-
-                            <h2>REGISTER</h2>
-                            <form onSubmit={onRegister}>
-                                <fieldset>
-                                    <div className="row">
-                                        <div className="col-sm-12 col-md-7">
-                                            <FormGroup label="Username" id="username_1">
-                                                <input type="text" className="form-control" placeholder="Username" aria-label="Username" />
-                                            </FormGroup>
+                            ) : null }
+                            { login_choice === 'register' ? (
+                                <div>
+                                    <h2>REGISTER</h2>
+                                    <form onSubmit={onRegister}>
+                                        <fieldset>
+                                            <div className="row">
+                                                <div className="col-sm-12 col-md-7">
+                                                    <FormGroup label="Username" id="username_1">
+                                                        <input type="text" className="form-control" placeholder="Username" aria-label="Username" />
+                                                    </FormGroup>
+                                                </div>
+                                                <div className="col-sm-12 col-md-7">
+                                                    <FormGroup label="Email" id="email">
+                                                        <input type="email" className="form-control" placeholder="Email" aria-label="Email" />
+                                                    </FormGroup>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="col-sm-12 col-md-7">
+                                                    <FormGroup label="Password" id="password_1">
+                                                        <input type="password" className="form-control" placeholder="Password" aria-label="Password" />
+                                                    </FormGroup>
+                                                </div>
+                                                <div className="col-sm-12 col-md-7">
+                                                    <FormGroup label="Confirm Password" id="password_2">
+                                                        <input type="password" className="form-control" placeholder="Confirm Password" aria-label="Confirm Password" />
+                                                    </FormGroup>
+                                                </div>
+                                            </div>
+                                            <div className="row">
+                                                <div className="col-sm-12 col-md-7">
+                                                    <FormGroup label="First Name" id="first_name">
+                                                        <input type="text" className="form-control" placeholder="First Name" aria-label="First Name" />
+                                                    </FormGroup>
+                                                </div>
+                                                <div className="col-sm-12 col-md-7">
+                                                    <FormGroup label="Last Name" id="last_name">
+                                                        <input type="text" className="form-control" placeholder="Last Name" aria-label="Last Name" />
+                                                    </FormGroup>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                        <div className="row">
+                                            <div className="col-sm-12 col-md-offset-3 col-xs-6 col-md-offset-5">
+                                                <button className="btn btn-lex">Register</button>
+                                            </div>
                                         </div>
-                                        <div className="col-sm-12 col-md-7">
-                                            <FormGroup label="Email" id="email">
-                                                <input type="email" className="form-control" placeholder="Email" aria-label="Email" />
-                                            </FormGroup>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-sm-12 col-md-7">
-                                            <FormGroup label="Password" id="password_1">
-                                                <input type="password" className="form-control" placeholder="Password" aria-label="Password" />
-                                            </FormGroup>
-                                        </div>
-                                        <div className="col-sm-12 col-md-7">
-                                            <FormGroup label="Confirm Password" id="password_2">
-                                                <input type="password" className="form-control" placeholder="Confirm Password" aria-label="Confirm Password" />
-                                            </FormGroup>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-sm-12 col-md-7">
-                                            <FormGroup label="First Name" id="first_name">
-                                                <input type="text" className="form-control" placeholder="First Name" aria-label="First Name" />
-                                            </FormGroup>
-                                        </div>
-                                        <div className="col-sm-12 col-md-7">
-                                            <FormGroup label="Last Name" id="last_name">
-                                                <input type="text" className="form-control" placeholder="Last Name" aria-label="Last Name" />
-                                            </FormGroup>
-                                        </div>
-                                    </div>
-                                </fieldset>
-                                <div className="row">
-                                    <div className="col-sm-12 col-md-offset-3 col-xs-6 col-md-offset-5">
-                                        <button className="btn btn-lex">Register</button>
-                                    </div>
+                                    </form>
                                 </div>
-                            </form>
+                            ) : null }
                         </div>
                     </div>
                 </div>
@@ -149,6 +169,21 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
+        onComponentDidMount() {
+            dispatch(formInit());
+            const login_init = {
+                login_choice: 'login',
+            };
+            dispatch(formUpdate(login_init));
+        },
+        changeSelection() {
+            return (e) => {
+                const login_selection = {
+                    login_choice: e.target.id,
+                };
+                dispatch(formUpdate(login_selection));
+            };
+        },
         onLogin(event) {
             event.preventDefault();
             dispatch(login())
