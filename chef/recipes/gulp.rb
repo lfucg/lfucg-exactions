829ab86a1e30bb00b30de5bea2243f51e4a23060
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-07-18 15:20:25 -0400

    Update gulp.rb recipe to initiate client side.

--- chef/recipes/gulp.rb
+++ chef/recipes/gulp.rb
@@ -9,15 +9,30 @@
 
 include_recipe "nodejs::npm"
 
+# if Dir.exists? "/home/ubuntu"
+#     directory "/home/ubuntu/lfucg-exactions/lfucg-exactions/client/node_modules" do
+#       owner 'ubuntu'
+#       group 'ubuntu'
+#       only_if { node['symlink_npm'] }
+#     end
+
+#     link "/home/ubuntu/lfucg-exactions/lfucg-exactions/client/node_modules" do
+#       owner 'ubuntu'
+#       group 'ubuntu'
+#       to "/home/ubuntu/lfucg-exactions/lfucg-exactions/client/node_modules"
+#       only_if { node['symlink_npm'] }
+#     end
+# end
+
 nodejs_npm "npm-install" do
-  path "/home/ubuntu/educator-certifications/client"
-  user "ubuntu"
+  path "/home/ubuntu/lfucg-exactions/lfucg-exactions/client"
+  user 'ubuntu'
   json true
 end
 
-execute "gulp-webapp" do
-  cwd "/home/ubuntu/educator-certifications/client"
-  command "npm run build"
+execute "gulp-build" do
+  cwd "/home/ubuntu/lfucg-exactions/lfucg-exactions/client"
+  command "npm build"
   environment "HOME" => "/home/ubuntu"
   user 'ubuntu'
   group 'ubuntu'
