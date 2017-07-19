46ae8b7f3ba74f3365ee944ce3035b528431eaf9
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-07-19 12:20:05 -0400

    Add health check template server side and update initial_data json.

--- lfucg-exactions/server/base/views.py
+++ lfucg-exactions/server/base/views.py
@@ -2,7 +2,12 @@
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 class IndexView(TemplateView):
     template_name = 'base/index.html'
 
+@method_decorator(csrf_exempt, name='dispatch')
+class HealthView(TemplateView):
+    template_name = 'base/health.html'
