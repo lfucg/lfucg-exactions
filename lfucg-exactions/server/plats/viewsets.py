c23c15cf2f9bad0b7b70117f45b148f61e6276bc
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-07-25 10:14:11 -0400

    Search Lots.

--- lfucg-exactions/server/plats/viewsets.py
+++ lfucg-exactions/server/plats/viewsets.py
@@ -15,6 +15,23 @@ class LotViewSet(viewsets.ModelViewSet):
     serializer_class = LotSerializer
     queryset = Lot.objects.all()
 
+    def get_queryset(self):
+        queryset = Lot.objects.order_by('address_street')
+
+        query_text = self.request.query_params.get('query', None)
+        if query_text is not None:
+            query_text = query_text.lower()
+            tmp_set = queryset.filter(address_full__icontains=query_text)
+            tmp_set |= queryset.filter(lot_number__icontains=query_text)
+            tmp_set |= queryset.filter(parcel_id__icontains=query_text)
+            tmp_set |= queryset.filter(permit_id__icontains=query_text)
+            tmp_set |= queryset.filter(plat__expansion_area__icontains=query_text)
+
+            queryset = tmp_set
+
+        return queryset.order_by('address_street')
+
+
 class PlatZoneViewSet(viewsets.ModelViewSet):
     serializer_class = PlatZoneSerializer
     queryset = PlatZone.objects.all()
