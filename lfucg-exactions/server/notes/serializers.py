59504679e1c5894ae8935fb922fadeb060ade4a1
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-07-27 15:52:01 -0400

    Create Notes Component with api call structure.

--- lfucg-exactions/server/notes/serializers.py
+++ lfucg-exactions/server/notes/serializers.py
@@ -11,9 +11,6 @@ class NoteSerializer(serializers.ModelSerializer):
             'user',
             'note',
             'date',
-            # 'content_type',
-            # 'object_id',
-            # 'content_object',
         )
 
 class RateSerializer(serializers.ModelSerializer):
