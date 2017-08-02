566cb3ce0e455f6dd15a59fb9403bb5df0af12d9
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-08-02 12:32:12 -0400

    Update SubdivisionExisting and remove landing pages.

--- lfucg-exactions/client/js/dashboard.js
+++ lfucg-exactions/client/js/dashboard.js
@@ -13,21 +13,18 @@ import Registration from './components/RegistrationPage';
 import ForgotPassword from './components/ForgotPassword';
 import ForgotUsername from './components/ForgotUsername';
 
-import SubdivisionPage from './components/SubdivisionPage';
 import SubdivisionExisting from './components/SubdivisionExisting';
 import SubdivisionForm from './components/SubdivisionForm';
 
-import PlatPage from './components/PlatPage';
 import PlatExisting from './components/PlatExisting';
 import PlatForm from './components/PlatForm';
 import PlatSummary from './components/PlatSummary';
 
-import LotPage from './components/LotPage';
 import LotExisting from './components/LotExisting';
 import LotForm from './components/LotForm';
 
 global.reduxStore = dashboardStore;
-global.BASE_STATIC_URL = window.location.host === '52.201.224.95' ?
+global.BASE_STATIC_URL = (window.location.host === '52.201.224.95') ?
     //'52.201.224.95' :
     'https://s3.amazonaws.com/chef-lfucg/temp_exactions_images' :
     '/static';
@@ -46,18 +43,15 @@ ReactDOM.render(
             <Route path="forgot-password/" component={ForgotPassword} name="Forgot Password" />
             <Route path="forgot-username/" component={ForgotUsername} name="Forgot Username" />
 
-            <Route path="subdivision" component={SubdivisionPage} name="Subdivision" />
             <Route path="subdivision/existing" component={SubdivisionExisting} name="Existing Subdivisions" />
             <Route path="subdivision/form" component={SubdivisionForm} name="Subdivision Form" />
             <Route path="subdivision/form/:id" component={SubdivisionForm} name="Current Subdivision Form" />
 
-            <Route path="plat" component={PlatPage} name="Plat" />
             <Route path="plat/existing" component={PlatExisting} name="Existing Plats" />
             <Route path="plat/summary/:id" component={PlatSummary} name="Plat Summary" />
             <Route path="plat/form" component={PlatForm} name="Plat Form" />
             <Route path="plat/form/:id" component={PlatForm} name="Current Plat Form" />
 
-            <Route path="lot" component={LotPage} name="Lot" />
             <Route path="lot/existing" component={LotExisting} name="Existing Lots" />
             <Route path="lot/form" component={LotForm} name="Lot Form" />
             <Route path="plat/:id/lot/form" component={LotForm} name="Lot Form with Plat" />
