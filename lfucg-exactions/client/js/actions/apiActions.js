623dbb8703e8dc0edec0aada9c6724f03717c4f5
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-09-06 10:34:35 -0400

    Delete unused API calls.

--- lfucg-exactions/client/js/actions/apiActions.js
+++ lfucg-exactions/client/js/actions/apiActions.js
@@ -6,9 +6,6 @@ import {
     ME,
 
     LOGIN,
-    // REGISTER,
-    // PASSWORD,
-    // SEND_USERNAME,
     LOGOUT,
 
     GET_SUBDIVISIONS,
@@ -115,76 +112,6 @@ export function login() {
         },
     };
 }
-
-// export function register() {
-//     return {
-//         type: API_CALL,
-//         endpoint: REGISTER,
-//         url: '/register/',
-//         method: 'POST',
-//         body: (getState) => {
-//             const {
-//                 activeForm,
-//             } = getState();
-//             const {
-//                 username_1,
-//                 password_1,
-//                 // password_2,
-//                 first_name,
-//                 last_name,
-//                 email,
-//             } = activeForm;
-//             return {
-//                 username: username_1,
-//                 password_1,
-//                 // password_2,
-//                 first_name,
-//                 last_name,
-//                 email,
-//             };
-//         },
-//     };
-// }
-
-// export function passwordReset() {
-//     return {
-//         type: API_CALL,
-//         endpoint: PASSWORD,
-//         url: '/forgot-password/',
-//         method: 'POST',
-//         body: (getState) => {
-//             const {
-//                 activeForm,
-//             } = getState();
-//             const {
-//                 email_2,
-//             } = activeForm;
-//             return {
-//                 email: email_2,
-//             };
-//         },
-//     };
-// }
-
-// export function sendUsername() {
-//     return {
-//         type: API_CALL,
-//         endpoint: SEND_USERNAME,
-//         url: '/forgot-username/',
-//         method: 'POST',
-//         body: (getState) => {
-//             const {
-//                 activeForm,
-//             } = getState();
-//             const {
-//                 email_3,
-//             } = activeForm;
-//             return {
-//                 email: email_3,
-//             };
-//         },
-//     };
-// }
 
 export function logout() {
     return {
