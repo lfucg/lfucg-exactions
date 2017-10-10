0e4e97ebf207b875bf462ed368b64d81f43e5721
Kelly Wright <kelly.wright@kellyinnovation.com>
2017-10-09 22:01:36 -0400

    Add read only fields to plats related admins.

--- lfucg-exactions/server/plats/admin.py
+++ lfucg-exactions/server/plats/admin.py
@@ -3,6 +3,19 @@ from simple_history.admin import SimpleHistoryAdmin
 
 from plats.models import *
 
+class SubdivisionHistoryAdmin(SimpleHistoryAdmin):
+    list_display = (
+        'name',
+        'gross_acreage',
+        'id',
+    )
+    readonly_fields = (
+        'created_by',
+        'modified_by',
+        'date_created',
+        'date_modified',
+    )
+
 class PlatHistoryAdmin(SimpleHistoryAdmin):
     list_display = (
         'name',
@@ -14,19 +27,62 @@ class PlatHistoryAdmin(SimpleHistoryAdmin):
         'block',
         'section',
         'total_acreage',
-        'sewer_due',
-        'non_sewer_due',
     )
+    readonly_fields = (
+        'created_by',
+        'modified_by',
+        'date_created',
+        'date_modified',
+    )
+
+    def subdivision(self, obj):
+        return obj.subdivision.name
+    subdivision.short_description = 'Subdivision'
+
+    def account(self, obj):
+        return obj.account.account_name
+    account.short_description = 'Account'
 
 class LotHistoryAdmin(SimpleHistoryAdmin):
     list_display = (
         'address_full',
         'id',
         'plat',
+        'lot_number',
+        'permit_id',
+    )
+    readonly_fields = (
+        'created_by',
+        'modified_by',
+        'date_created',
+        'date_modified',
     )
 
-admin.site.register(Subdivision, SimpleHistoryAdmin)
+    def plat(self, obj):
+        return obj.plat.name
+    plat.short_description = 'Plat'
+
+class PlatZoneHistoryAdmin(SimpleHistoryAdmin):
+    list_display = (
+        'zone',
+        'acres',
+        'id',
+        'plat',
+    )
+    readonly_fields = (
+        'created_by',
+        'modified_by',
+        'date_created',
+        'date_modified',
+    )
+
+    def plat(self, obj):
+        return obj.plat.name
+    plat.short_description = 'Plat'
+
+
+admin.site.register(Subdivision, SubdivisionHistoryAdmin)
 admin.site.register(Plat, PlatHistoryAdmin)
 admin.site.register(Lot, LotHistoryAdmin)
-admin.site.register(PlatZone, SimpleHistoryAdmin)
+admin.site.register(PlatZone, PlatZoneHistoryAdmin)
 admin.site.register(CalculationWorksheet, SimpleHistoryAdmin)
