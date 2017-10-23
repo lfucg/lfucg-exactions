c6154cb58e10de0f79be0dde365446591cd67aba
JMStewart00 <josh@apaxsoftware.com>
2017-10-23 11:48:57 -0400

    Code cleanup

--- lfucg-exactions/client/js/components/AccountExisting.js
+++ lfucg-exactions/client/js/components/AccountExisting.js
@@ -111,6 +111,7 @@ class AccountExisting extends React.Component {
                     { filterField: 'filter_lot_account__id', displayName: 'Lot', list: lotsList },
                   ]}
                 />
+
                 <div className="inside-body">
                     <div className="container">
                         {accounts_list}
