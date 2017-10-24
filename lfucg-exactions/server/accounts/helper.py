0e5ec6e88e37b497cf62dbd8b8cdf409e72f609c
JMStewart00 <josh@apaxsoftware.com>
2017-10-24 12:51:26 -0400

    added signals for changing approved status

--- lfucg-exactions/server/accounts/helper.py
+++ lfucg-exactions/server/accounts/helper.py
@@ -1,8 +1,14 @@
 from django.conf import settings
 from django.template.loader import get_template
-
+from rest_framework import viewsets, status, filters
+from django.db.models import Q
 from django.core.mail import EmailMultiAlternatives
-from django.core.mail import mail_admins
+from django.contrib.auth.models import User
+
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from plats.models import *
+from .models import *
 
 def send_password_reset_email(user):
     text_template = get_template('emails/password_reset.txt')
@@ -39,3 +45,18 @@ def send_lost_username_email(user):
     msg = EmailMultiAlternatives(subject, text_content, from_email, [user.email])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
+
+@receiver(pre_save, sender=Agreement)
+@receiver(pre_save, sender=AccountLedger)
+@receiver(pre_save, sender=Payment)
+@receiver(pre_save, sender=Project)
+@receiver(pre_save, sender=ProjectCostEstimate)
+@receiver(pre_save, sender=Plat)
+@receiver(pre_save, sender=Lot)
+def set_approval(sender, instance, **kwargs):
+    if instance.modified_by.is_superuser == True or (hasattr(instance.modified_by, 'profile') and instance.modified_by.profile.is_supervisor == True):
+        instance.is_approved = True
+    else:
+        instance.is_approved = False
+    print(vars(instance))
+    return instance
