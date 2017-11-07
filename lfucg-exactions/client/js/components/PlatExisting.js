b0bb91f63fa2d9f15fc5824cbf61568474ba943e
JMStewart00 <josh@apaxsoftware.com>
2017-11-07 16:02:04 -0500

    Kelly review changes

--- lfucg-exactions/client/js/components/PlatExisting.js
+++ lfucg-exactions/client/js/components/PlatExisting.js
@@ -108,7 +108,7 @@ class PlatExisting extends React.Component {
                                 <div className="col-sm-offset-1">
                                     <p className="col-xs-12 col-sm-6">Sewer Due: {plat.plat_exactions && plat.plat_exactions.plat_sewer_due}</p>
                                     <p className="col-xs-12 col-sm-6">Non-Sewer Due: {plat.plat_exactions && plat.plat_exactions.plat_non_sewer_due}</p>
-                                    <p className="col-xs-6">Cabinet-Slide: {plat.cabinet}-{plat.slide}</p>
+                                    <p className="col-xs-6">Name: {plat.name}</p>
                                     <p className="col-xs-6">Section: {plat.section}</p>
                                     <p className="col-xs-6">Block: {plat.block}</p>
                                     <p className="col-xs-6">Expansion Area: {plat.expansion_area}</p>
