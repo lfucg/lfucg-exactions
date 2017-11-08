5ed602f6ab2f3a17e2386c2b42bf3b46f0480ed8
JMStewart00 <josh@apaxsoftware.com>
2017-11-08 15:03:28 -0500

    reverting changes

--- lfucg-exactions/client/js/components/Notes.js
+++ lfucg-exactions/client/js/components/Notes.js
@@ -125,7 +125,7 @@ class Notes extends React.Component {
 
 Notes.propTypes = {
     currentUser: PropTypes.object,
-    notes: PropTypes.array,
+    notes: PropTypes.object,
     onComponentDidMount: PropTypes.func,
     onSubmit: PropTypes.func,
     ariaExpanded: PropTypes.string,
