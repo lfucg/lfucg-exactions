a58ca56246ab573c96841a68462af7f3ad48feaf
JMStewart00 <josh@apaxsoftware.com>
2017-11-13 14:25:59 -0500

    fixed problems with flash messaging and added safety check for classList

--- lfucg-exactions/client/js/middlewares/flashMiddleware.js
+++ lfucg-exactions/client/js/middlewares/flashMiddleware.js
@@ -2,6 +2,7 @@ import { map } from 'ramda';
 
 import {
     flashMessageClear,
+    flashMessageSet,
 } from '../actions/flashMessageActions';
 
 import {
@@ -18,11 +19,15 @@ export default function flashMiddleware({ dispatch }) {
         }
         if (action.type === ERROR_MESSAGE_SET) {
             map((error) => {
-                document.getElementById(error).classList.add('error');
-                document.querySelector(`label[for='${error}']`).classList.add('label-error');
-                const help_text = document.getElementById(`help-block-${error}`);
-                help_text.classList.remove('hidden');
-                help_text.innerHTML = `<i class="fa fa-exclamation-circle"></i> ${action.response[error]}`;
+                if (error !== 'non_field_errors') {
+                    console.log('hey why are we in here?');
+                    console.log('ERROR FIELD TYPE IS: ', typeof error);
+                    document.getElementById(error).classList.add('error');
+                    document.querySelector(`label[for='${error}']`).classList.add('label-error');
+                    const help_text = document.getElementById(`help-block-${error}`);
+                    help_text.classList.remove('hidden');
+                    help_text.innerHTML = `<i class="fa fa-exclamation-circle"></i> ${action.response[error]}`;
+                }
             })(Object.keys(action.response));
         }
         return next(action);
