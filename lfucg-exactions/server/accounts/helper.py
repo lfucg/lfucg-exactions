a729a84d5eac7bc4f0c0ffbd5d57a78faf11e7c5
Kelly Wright <kelly.wright@kellyinnovation.com>
2018-11-08 14:54:17 -0500

    Uncomment helper code.

--- lfucg-exactions/server/accounts/helper.py
+++ lfucg-exactions/server/accounts/helper.py
@@ -53,75 +53,75 @@ def send_lost_username_email(user):
     msg.attach_alternative(html_content, "text/html")
     msg.send()
 
-# @receiver(post_save, sender=User)
-# def send_email_to_new_user(sender, instance, **kwargs):
-#     user = instance
-#     token = PasswordResetTokenGenerator().make_token(user)
+@receiver(post_save, sender=User)
+def send_email_to_new_user(sender, instance, **kwargs):
+    user = instance
+    token = PasswordResetTokenGenerator().make_token(user)
 
-#     text_template = get_template('emails/password_reset.txt')
-#     html_template = get_template('emails/password_reset.html')
+    text_template = get_template('emails/password_reset.txt')
+    html_template = get_template('emails/password_reset.html')
 
-#     subject, from_email = 'LFUCG Exactions: Password Reset', settings.DEFAULT_FROM_EMAIL
+    subject, from_email = 'LFUCG Exactions: Password Reset', settings.DEFAULT_FROM_EMAIL
 
-#     context = {
-#         'user': user,
-#         'baseURL': settings.BASE_URL,
-#         'uid': int_to_base36(user.id),
-#         'token': token,
-#     }
+    context = {
+        'user': user,
+        'baseURL': settings.BASE_URL,
+        'uid': int_to_base36(user.id),
+        'token': token,
+    }
 
-#     text_content = text_template.render(context)
-#     html_content = html_template.render(context)
+    text_content = text_template.render(context)
+    html_content = html_template.render(context)
 
-#     msg = EmailMultiAlternatives(subject, text_content, from_email, [user.email])
-#     msg.attach_alternative(html_content, "text/html")
-#     try:
-#         msg.send()
-#     except Exception as exc:
-#         print('SEND NEW USER EMAIL EXCEPTION', exc)
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [user.email])
+    msg.attach_alternative(html_content, "text/html")
+    try:
+        msg.send()
+    except Exception as exc:
+        print('SEND NEW USER EMAIL EXCEPTION', exc)
 
-# @receiver(post_save, sender=Agreement)
-# @receiver(post_save, sender=AccountLedger)
-# @receiver(post_save, sender=Payment)
-# @receiver(post_save, sender=Project)
-# @receiver(post_save, sender=ProjectCostEstimate)
-# @receiver(post_save, sender=Plat)
-# @receiver(post_save, sender=Lot)
-# def send_email_to_supervisors(sender, instance, created=False, **kwargs):
+@receiver(post_save, sender=Agreement)
+@receiver(post_save, sender=AccountLedger)
+@receiver(post_save, sender=Payment)
+@receiver(post_save, sender=Project)
+@receiver(post_save, sender=ProjectCostEstimate)
+@receiver(post_save, sender=Plat)
+@receiver(post_save, sender=Lot)
+def send_email_to_supervisors(sender, instance, created=False, **kwargs):
 
-#     if instance.modified_by.is_superuser or (hasattr(instance.modified_by, 'profile') and instance.modified_by.profile.is_supervisor == True):
-#         return
+    if instance.modified_by.is_superuser or (hasattr(instance.modified_by, 'profile') and instance.modified_by.profile.is_supervisor == True):
+        return
 
-#     ctype = ContentType.objects.get_for_model(instance)
+    ctype = ContentType.objects.get_for_model(instance)
 
-#     if ctype.app_label == 'accounts':
-#         group = ['Finance']
-#     elif ctype.app_label == 'plats':
-#         group = ['Planning']
+    if ctype.app_label == 'accounts':
+        group = ['Finance']
+    elif ctype.app_label == 'plats':
+        group = ['Planning']
 
-#     users = User.objects.filter(Q(profile__is_supervisor=True) & Q(groups__name__in=group))
+    users = User.objects.filter(Q(profile__is_supervisor=True) & Q(groups__name__in=group))
 
-#     to_emails = list(users.values_list('email', flat=True))
+    to_emails = list(users.values_list('email', flat=True))
 
-#     html_template = get_template('emails/supervisor_email.html')
-#     text_template = get_template('emails/supervisor_email.txt')
+    html_template = get_template('emails/supervisor_email.html')
+    text_template = get_template('emails/supervisor_email.txt')
 
-#     subject = 'LFUCG Exactions Activity: New Entry Pending Approval'
-#     from_email = settings.DEFAULT_FROM_EMAIL
+    subject = 'LFUCG Exactions Activity: New Entry Pending Approval'
+    from_email = settings.DEFAULT_FROM_EMAIL
 
-#     context = {
-#         'baseURL': settings.BASE_URL,
-#         'model': ctype.model,
-#         'staticURL': settings.STATIC_URL,
-#         'id': instance.id,
-#     }
+    context = {
+        'baseURL': settings.BASE_URL,
+        'model': ctype.model,
+        'staticURL': settings.STATIC_URL,
+        'id': instance.id,
+    }
 
-#     html_content = html_template.render(context)
-#     text_content = text_template.render(context)
+    html_content = html_template.render(context)
+    text_content = text_template.render(context)
 
-#     msg = EmailMultiAlternatives(subject, text_content, from_email, to_emails)
-#     msg.attach_alternative(html_content, "text/html")
-#     msg.send()
+    msg = EmailMultiAlternatives(subject, text_content, from_email, to_emails)
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
 
 @receiver(pre_save, sender=Agreement)
 @receiver(pre_save, sender=AccountLedger)
