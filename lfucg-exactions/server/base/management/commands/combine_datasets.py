5eb03c0cefa3772b015c6b7a44fabc19f40d441e
Kelly Wright <kelly.wright@kellyinnovation.com>
2018-11-08 17:16:16 -0500

    Reduce new dataframe before concat.

--- lfucg-exactions/server/base/management/commands/combine_datasets.py
+++ lfucg-exactions/server/base/management/commands/combine_datasets.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 def combine():
   df_hist = pd.read_csv('lot_data.csv')
-  
+
   df_new = pd.read_csv('oct_2018.csv')
   df_new.insert(0, 'New', 'New')
   df_non_repeat = df_new[~df_new['AddressID'].isin(df_hist['AddressID'])]
 
-  df_combined = pd.concat([df_hist, df_non_repeat], keys="AddressID", sort=False)
+  df_combined = pd.concat([df_hist, df_non_repeat], sort=False)
   print('COMBINED', df_combined[:10])
 
   writer = pd.ExcelWriter('Combined Data.xlsx')
@@ -16,13 +16,11 @@ def combine():
 
   writer.save()
 
-  # duplicates = df_combined[df_combined['AddressID'].duplicated()]['AddressID']
-
-  # df_drop_dupl = df_combined[(df_combined['AddressID'].isin(duplicates) & (df_combined['New'] == 'New'))]
-
-
 # For use in shell_plus
 # from base.management.commands.combine_datasets import combine
+
+
+
 
 
 # def get_undefined_ledgers():
