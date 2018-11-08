119c960ec5fc2156bd86c825731366106d205da4
Kelly Wright <kelly.wright@kellyinnovation.com>
2018-11-08 14:54:17 -0500

    Uncomment helper code.

--- chef/attributes/default.rb
+++ chef/attributes/default.rb
@@ -2,7 +2,7 @@ node.default['nodejs']['install_method'] = 'binary'
 node.default['nodejs']['version'] = '8.12.0'
 node.default['nodejs']['binary']['checksum'] = '3df19b748ee2b6dfe3a03448ebc6186a3a86aeab557018d77a0f7f3314594ef6'
 
-node.default['nodejs']['npm']['version'] = '4.5.0' # Kelly Play with the version
+node.default['nodejs']['npm']['version'] = '4.5.0' 
 
 node.default['poise-python']['install_python2'] = false
 node.default['poise-python']['options']['pip_version'] = '18.0'
